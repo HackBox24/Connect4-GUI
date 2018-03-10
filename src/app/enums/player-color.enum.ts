1627df58cea525af72fb104b690534b3fb369033
Aaron Osher <aaron@oshers.com>
2018-03-10 15:23:12 +0000

    Add Game Model

---
+++ src/app/enums/player-color.enum.ts
@@ -0,0 +1,6 @@
+export enum PlayerColor {
+  Red = '#db2828',
+  Green = '#21ba45',
+  Yellow = '#fbbd08',
+  Blue = '#2185d0'
+}
---
+++ src/app/models/game-model.ts
@@ -0,0 +1,71 @@
+import {PlayerColor} from '../enums/player-color.enum';
+
+export interface GameModel {
+  code: string[6]; // This is al the game's UID
+  player1: string | null;
+  player1Color: PlayerColor | null;
+  player2: string | null;
+  player2Color: PlayerColor | null;
+  player3: string | null;
+  player3Color: PlayerColor | null;
+  player4: string | null;
+  player4Color: PlayerColor | null;
+  player_count: number;
+  players: string[];
+  board: {
+    1: {
+      1: string | null;
+      2: string | null;
+      3: string | null;
+      4: string | null;
+      5: string | null;
+      6: string | null;
+      7: string | null;
+    },
+    2: {
+      1: string | null;
+      2: string | null;
+      3: string | null;
+      4: string | null;
+      5: string | null;
+      6: string | null;
+      7: string | null;
+    },
+    3: {
+      1: string | null;
+      2: string | null;
+      3: string | null;
+      4: string | null;
+      5: string | null;
+      6: string | null;
+      7: string | null;
+    },
+    4: {
+      1: string | null;
+      2: string | null;
+      3: string | null;
+      4: string | null;
+      5: string | null;
+      6: string | null;
+      7: string | null;
+    },
+    5: {
+      1: string | null;
+      2: string | null;
+      3: string | null;
+      4: string | null;
+      5: string | null;
+      6: string | null;
+      7: string | null;
+    },
+    6: {
+      1: string | null;
+      2: string | null;
+      3: string | null;
+      4: string | null;
+      5: string | null;
+      6: string | null;
+      7: string | null;
+    }
+  };
+}
