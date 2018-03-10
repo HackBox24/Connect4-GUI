577de65d997abb77da5d6b239ad480ef6e146652
Aaron Osher <aaron@oshers.com>
2018-03-10 14:12:53 +0000

    Add Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true
+  firebase: {
+    apiKey: 'AIzaSyDHuiVLExveFj22DEtm0j_9Ai1iLoe2odw',
+    authDomain: 'hack-box-c4.firebaseapp.com',
+    databaseURL: 'https://hack-box-c4.firebaseio.com',
+    projectId: 'hack-box-c4',
+    storageBucket: '',
+    messagingSenderId: '738355889947'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDHuiVLExveFj22DEtm0j_9Ai1iLoe2odw',
+    authDomain: 'hack-box-c4.firebaseapp.com',
+    databaseURL: 'https://hack-box-c4.firebaseio.com',
+    projectId: 'hack-box-c4',
+    storageBucket: '',
+    messagingSenderId: '738355889947'
+  }
 };
