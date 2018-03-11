fb9ee409ad91450f2aefc52064b0d6db2282d197
Aaron Osher <aaron@oshers.com>
2018-03-11 01:15:42 +0000

    Game creation sorted

--- src/app/pages/create/create.component.ts
+++ src/app/pages/create/create.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {AngularFirestore} from 'angularfire2/firestore';
 import {PlayerColor} from '../../enums/player-color.enum';
 import {Router} from '@angular/router';
+import {GameService} from '../../services/game.service';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-create',
@@ -12,13 +14,22 @@ export class CreateComponent implements OnInit {
 
   constructor(
     private db: AngularFirestore,
-    private router: Router
+    private router: Router,
+    private games: GameService,
+    private auth: AuthService
   ) { }
 
   ngOnInit() {
-      //
-      // .then(() => this.router.navigate(['play', id]))
-      // .catch(error => console.error(error));
+    this.auth.login();
+    this.auth.authState
+      .subscribe(authState => {
+        console.log(authState);
+        if (authState !== null) {
+          this.games.create(authState.uid)
+            .then(game => this.router.navigate(['play', game]))
+            .catch(error => console.error(error));
+        }
+      });
   }
 
 }
