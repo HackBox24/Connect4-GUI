7f3521d45f2bc1a6bafd35cf4f6e22601233b01f
Aaron Osher <aaron@oshers.com>
2018-03-11 01:16:47 +0000

    Update Play Component Add Loader

--- src/app/pages/play/play.component.ts
+++ src/app/pages/play/play.component.ts
@@ -14,7 +14,7 @@ import {GameModel} from '../../models/game-model';
 })
 export class PlayComponent implements OnInit {
 
-  $game = Observable<GameModel | null>;
+  $game: Observable<GameModel | null>;
 
   constructor(
     private route: ActivatedRoute,
@@ -32,73 +32,3 @@ export class PlayComponent implements OnInit {
   }
 
 }
-
-const GAME = {
-  code: 'hjx21',
-  player1: 'player1',
-  player1Color: PlayerColor.Red,
-  player2: 'player2',
-  player2Color: PlayerColor.Green,
-  player3: null,
-  player3Color: null,
-  player4: null,
-  player4Color: null,
-  player_count: 2,
-  players: ['player1', 'player2'],
-  board: {
-    1: {
-      1: null,
-      2: 'player1',
-      3: null,
-      4: 'player2',
-      5: 'player1',
-      6: null,
-      7: null,
-    },
-    2: {
-      1: null,
-      2: null,
-      3: null,
-      4: null,
-      5: null,
-      6: null,
-      7: null,
-    },
-    3: {
-      1: null,
-      2: null,
-      3: null,
-      4: null,
-      5: null,
-      6: null,
-      7: null,
-    },
-    4: {
-      1: null,
-      2: null,
-      3: null,
-      4: null,
-      5: null,
-      6: null,
-      7: null,
-    },
-    5: {
-      1: null,
-      2: null,
-      3: null,
-      4: null,
-      5: null,
-      6: null,
-      7: null,
-    },
-    6: {
-      1: null,
-      2: null,
-      3: null,
-      4: null,
-      5: null,
-      6: null,
-      7: null,
-    }
-  }
-};
